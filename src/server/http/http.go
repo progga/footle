b9d3c291b0481884bdc0d600d0fa8ef8f356d5a5
Progga <proggaprogga@gmail.com>
2017-05-27 19:16:25 +0000

    Adding first implementation of the HTTP interface.

--- src/server/http/http.go
+++ src/server/http/http.go
@@ -1,28 +1,232 @@
-
 /**
- * Provides the HTTP user interface for the debugger.
- *
- * *** Unimplemented. ***
+ * Provides the HTTP interface for the debugger.
  */
 
 package http
 
 import (
-  "../dbgp/message"
+	"../dbgp/command"
+	"../dbgp/message"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
 )
 
+const DOCROOT_PATH = "../../ui/"
+
+type client chan<- string
+
 /**
- *
+ * List of HTTP clients that are currently listening for Server sent events.
  */
-func RunUI(out chan<- string) {
+var clientList map[client]bool
+
+/**
+ * Initializes current HTTP client list.
+ */
+func init() {
+
+	clientList = make(map[client]bool)
 }
 
 /**
+ * Setup HTTP server.
  *
+ * Four handlers are used:
+ *   - HTTP interface for Footle.
+ *   - A file browser for selecting files that will be debugged.
+ *   - Debugging command receiver.  This is supposed to be called over Ajax.
+ *   - Debugging output sender.  This is supposed to be consumed using
+ *     Server sent events.
+ *
+ * Uses global variable "clientList."
  */
-func UpdateUIStatus(in <-chan message.Message) {
+func Listen(codeDir string, out chan string) {
 
-  for msg := range in {
-    _ = msg
-  }
+	uiPath, err := determineUIPath()
+	if nil != err {
+		log.Fatal(err)
+	}
+
+	arrival := make(chan client)
+	departure := make(chan client)
+	go manageClients(clientList, arrival, departure)
+
+	http.Handle("/", http.FileServer(http.Dir(uiPath)))
+	// Serve the files that will be debugged.
+	http.Handle("/files/", http.StripPrefix("/files/", http.FileServer(http.Dir(codeDir))))
+
+	http.HandleFunc("/steering-wheel", makeReceiveHandler(out))
+	http.HandleFunc("/message-stream", makeTransmitHandler(arrival, departure))
+
+	http.ListenAndServe(":9090", nil)
+}
+
+/**
+ * Pass on DBGp command output to HTTP clients.
+ *
+ * Uses global variable "clientList."
+ */
+func Tell(in <-chan message.Message) {
+
+	for msg := range in {
+		broadcast(fmt.Sprint(msg), clientList)
+	}
+}
+
+/**
+ * Wrapper for the "receive" handler.
+ *
+ * Apart from the usual arguments for an HTTP handler, it passes a channel
+ * to receiver().  This channel can be used to write whatever is received
+ * by receive().
+ */
+func makeReceiveHandler(out chan string) http.HandlerFunc {
+
+	return func(writeStream http.ResponseWriter, request *http.Request) {
+
+		receive(writeStream, request, out)
+	}
+}
+
+/**
+ * Processes HTTP POST calls to the "/steering-wheel" path.
+ *
+ * Extracts whatever is sent by HTTP clients and tries to prepare a DBGp
+ * command out of it.  This command is then written to the output channel so
+ * that it can be sent to the DBGp engine.
+ */
+func receive(writeStream http.ResponseWriter, request *http.Request, out chan string) {
+
+	msg := request.FormValue("msg")
+
+	shortCmd, cmdArgs, err := command.Break(msg)
+	if nil != err {
+		fmt.Fprintf(writeStream, "%s", err)
+
+		return
+	}
+
+	DBGpCmd, err := command.Prepare(shortCmd, cmdArgs)
+	if nil != err {
+		fmt.Fprintf(writeStream, "%s", err)
+
+		return
+	}
+
+	fmt.Fprintf(writeStream, "Got it.")
+
+	out <- DBGpCmd
+}
+
+/**
+ * Wrapper over transmit().
+ *
+ * In addition to the usual arguments for an HTTP handler, it passes two
+ * channels to transmit().
+ */
+func makeTransmitHandler(arrival, departure chan client) http.HandlerFunc {
+
+	return func(writeStream http.ResponseWriter, request *http.Request) {
+
+		transmit(writeStream, request, arrival, departure)
+	}
+}
+
+/**
+ * Serves the "/transmit" path.
+ *
+ * Passes the output of DBGp commands to HTTP clients as Server sent events.
+ * Also announces the arrival and departure of each HTTP client.
+ *
+ * For each client, a new channel is created.  This channel is then passed to
+ * the other parts of Footle that writes the output of DBGp commands to this
+ * channel.
+ */
+func transmit(writeStream http.ResponseWriter, request *http.Request, arrival, departure chan client) {
+
+	myEar := make(chan string)
+	arrival <- myEar
+
+	flusher, ok := writeStream.(http.Flusher)
+
+	if ok {
+		writeStream.Header().Set("Content-Type", "text/event-stream")
+		writeStream.Header().Set("Cache-control", "no-cache")
+		writeStream.Header().Set("Connection", "keep-alive")
+
+		flusher.Flush()
+	} else {
+		fmt.Fprintf(writeStream, "Unable to Flush.")
+
+		departure <- myEar
+		return
+	}
+
+	closedConnectionNotification := writeStream.(http.CloseNotifier).CloseNotify()
+
+	go func() {
+		<-closedConnectionNotification
+		departure <- myEar
+	}()
+
+	for msg := range myEar {
+		fmt.Fprintf(writeStream, "data: %s\n\n", msg)
+		flusher.Flush()
+	}
+
+	// Only relevant when myEar has closed before writeStream.
+	fmt.Fprintf(writeStream, "event: close\ndata: The end\n\n")
+}
+
+/**
+ * Writes a string message to all current client channels.
+ */
+func broadcast(msg string, httpClientList map[client]bool) {
+
+	for clientChannel, _ := range httpClientList {
+		clientChannel <- msg
+	}
+}
+
+/**
+ * Update the list of HTTP clients currently listening.
+ *
+ * When an HTTP client first starts listening for Server sent events, we
+ * add it as a new client and vice-versa.
+ */
+func manageClients(httpClientList map[client]bool, arrival, departure <-chan client) {
+
+	for {
+		select {
+		case clientChannel := <-arrival:
+			httpClientList[clientChannel] = true
+
+		case clientChannel := <-departure:
+			delete(httpClientList, clientChannel)
+			close(clientChannel)
+		}
+	}
+}
+
+/**
+ * Find document root of the HTML UI.
+ */
+func determineUIPath() (uiPath string, err error) {
+
+	binPath, err := os.Executable()
+	if nil != err {
+		return uiPath, err
+	}
+
+	realBinPath, err := filepath.EvalSymlinks(binPath)
+	if nil != err {
+		return uiPath, err
+	}
+
+	uiPath = realBinPath + "/" + DOCROOT_PATH
+
+	return uiPath, err
 }
---
+++ src/server/http/http_test.go
@@ -0,0 +1,227 @@
+/**
+ * Tests for the HTTP interface.
+ */
+
+package http
+
+import (
+	"net/http/httptest"
+	"net/url"
+	"strings"
+	"testing"
+	"time"
+)
+
+/**
+ * Mock ResponseRecorder.
+ *
+ * httptest.ResponseRecorder does not implement http.CloseNotifier.  So we use
+ * a wrapper over httptest.ResponseRecorder instead.  This wrapper satisfies
+ * http.CloseNotifier.  This response recorder is useful where the HTTP handler
+ * needs to know as soon as the HTTP connection closes.  This is particularly
+ * useful for long running HTTP connections as is served by transmit().
+ */
+type mockResponseRecorder struct {
+	*httptest.ResponseRecorder
+	closeNotify chan bool
+}
+
+func (writer mockResponseRecorder) CloseNotify() <-chan bool { return writer.closeNotify }
+func (writer mockResponseRecorder) Close()                   { writer.closeNotify <- true }
+func (writer mockResponseRecorder) Flush()                   {}
+
+/**
+ * Tests for receive().
+ *
+ * Tests both the output text and DBGp command coming out of receive().
+ */
+func TestReceive(t *testing.T) {
+
+	// Fail case. "foo" is an invalid command.
+	formValues := url.Values{"msg": {"foo"}}
+	formReader := strings.NewReader(formValues.Encode())
+	request := httptest.NewRequest("POST", "/steering-wheel", formReader)
+	request.Header.Add("Content-Type", "application/x-www-form-urlencoded; param=value")
+	writer := httptest.NewRecorder()
+	commands := make(chan string)
+
+	receive(writer, request, commands)
+
+	expectedResponse := "Unknown command."
+	response := writer.Body.String()
+
+	if expectedResponse != response {
+		t.Errorf("receive(foo) says: %s", response)
+	}
+
+	// Pass case that uses the "status" command.
+	formValues = url.Values{"msg": {"status"}}
+	formReader = strings.NewReader(formValues.Encode())
+	request = httptest.NewRequest("POST", "/steering-wheel", formReader)
+	request.Header.Add("Content-Type", "application/x-www-form-urlencoded; param=value")
+	writer = httptest.NewRecorder()
+	commands = make(chan string)
+
+	go receive(writer, request, commands)
+	DBGpCmd := <-commands
+
+	expectedResponse = "Got it."
+	response = writer.Body.String()
+
+	if expectedResponse != response {
+		t.Errorf("receive(status) says: %s", response)
+	}
+
+	expectedCmd := "status -i 1\x00"
+	if expectedCmd != DBGpCmd {
+		t.Errorf("receive(status) commanded: %s", DBGpCmd)
+	}
+
+	// Another pass case.  The DBGp transaction ID should increase.
+	writer = httptest.NewRecorder()
+	go receive(writer, request, commands)
+	DBGpCmd = <-commands
+
+	expectedResponse = "Got it."
+	response = writer.Body.String()
+
+	if expectedResponse != response {
+		t.Errorf("receive(status) says: %s", response)
+	}
+
+	expectedCmd = "status -i 2\x00"
+	if expectedCmd != DBGpCmd {
+		t.Errorf("receive(status) commanded: %s", DBGpCmd)
+	}
+}
+
+/**
+ * Tests for transmit().
+ *
+ * Tests the whole sequence of events for our Server-sent-event server.
+ */
+func TestTransmit(t *testing.T) {
+
+	request := httptest.NewRequest("GET", "/message-stream", nil)
+	writer := mockResponseRecorder{
+		ResponseRecorder: httptest.NewRecorder(),
+		closeNotify:      make(chan bool),
+	}
+
+	// HTTP client has arrived.
+	arrival := make(chan client)
+	departure := make(chan client)
+	go transmit(writer, request, arrival, departure)
+
+	ear0 := <-arrival
+	ear0 <- "Foo bar."
+
+	time.Sleep(time.Millisecond)
+	streamedOutput := writer.Body.String()
+
+	expected := "data: Foo bar.\n\n"
+	if expected != streamedOutput {
+		t.Errorf("transmit() said: %s", streamedOutput)
+	}
+
+	// HTTP client has departed.  Its channel should be sent for removal from
+	// client list...
+	writer.Close()
+	time.Sleep(time.Millisecond)
+
+	earChannel := <-departure
+	if ear0 != earChannel {
+		t.Error("transmit() did not deal with channel at departure.")
+	}
+
+	// ...and a closing message has been sent.
+	close(ear0)
+	time.Sleep(time.Millisecond)
+	streamedOutput = writer.Body.String()
+
+	expected = "data: Foo bar.\n\nevent: close\ndata: The end\n\n"
+	if expected != streamedOutput {
+		t.Errorf("At the end, transmit() said: %s", streamedOutput)
+	}
+}
+
+/**
+ * Tests for broadcast().
+ */
+func TestBroadcast(t *testing.T) {
+
+	httpClientList := make(map[client]bool)
+
+	ear0 := make(chan string)
+	ear1 := make(chan string)
+	ear2 := make(chan string)
+
+	httpClientList[ear0] = true
+	httpClientList[ear1] = true
+	httpClientList[ear2] = true
+
+	go broadcast("Foo", httpClientList)
+
+	var msg0, msg1, msg2 string
+
+	// Record what we have just heard.
+	for i := 0; i < 3; i++ {
+		select {
+		case msg0 = <-ear0:
+
+		case msg1 = <-ear1:
+
+		case msg2 = <-ear2:
+		}
+	}
+
+	if "Foo" != msg0 || "Foo" != msg1 || "Foo" != msg2 {
+		t.Errorf("Wrong broadcast: %s, %s, %s", msg0, msg1, msg2)
+	}
+}
+
+/**
+ * Tests for manageClients().
+ *
+ * Tests arrival and departure recording of HTTP clients.
+ */
+func TestManageClients(t *testing.T) {
+
+	httpClientList := make(map[client]bool)
+	arrival := make(chan client)
+	departure := make(chan client)
+
+	go manageClients(httpClientList, arrival, departure)
+
+	ear0 := make(chan string)
+	ear1 := make(chan string)
+	ear2 := make(chan string)
+
+	arrival <- ear0
+	// Sleep() is needed to give the manageClients() goroutine a chance update
+	// the client list.
+	time.Sleep(time.Millisecond)
+	if 1 != len(httpClientList) {
+		t.Error("manageClients() failed to record arrival.")
+	}
+
+	departure <- ear0
+	time.Sleep(time.Millisecond)
+	if 0 != len(httpClientList) {
+		t.Error("manageClients() failed to record departure.")
+	}
+
+	arrival <- ear1
+	arrival <- ear2
+	time.Sleep(time.Millisecond)
+	if 2 != len(httpClientList) {
+		t.Error("manageClients() failed to record two arrivals.")
+	}
+
+	departure <- ear1
+	departure <- ear2
+	time.Sleep(time.Millisecond)
+	if 0 != len(httpClientList) {
+		t.Error("manageClients() failed to record two departures.")
+	}
+}
