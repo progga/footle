7166414ff6acf8f1b71dea2eaf8b2ef28a2d7344
Progga <proggaprogga@gmail.com>
2017-05-27 19:24:36 +0000

    Added support for command line flags and arguments.
    Command line and HTTP interfaces of the debugger can now be selectively turned
    on and off by the use of command line flags: --cmdline, --nohttp.
    
    The docroot of the code to debug can also be specified using the --docroot flag
    and its argument.  By default, we assume that this docroot is the same as the
    current working directory.

--- src/server/footle.go
+++ src/server/footle.go
@@ -3,12 +3,13 @@
  * Footle the DBGp debugger.
  *
  * Here we launch go routines for command line UI, HTTP UI, receiving
- * messages from DBGp engine and sending DBGp command to DBGp engine.
+ * messages from DBGp engine and sending DBGp commands to DBGp engine.
  */
 
 package main
 
 import (
+  "flag"
   "net"
   "./cmdline"
   "./core"
@@ -19,28 +20,69 @@ import (
 /**
  * Launch go routines.
  *
- * Launch the debugging process and its user interfaces.
+ * Launch the debugger and its user interfaces.
  *
  * End execution when the "bye" channel is closed.
  */
 func main() {
 
+  // Setup command line flags and arguments.
+  docroot, hasCmdLine, hasHTTP := getFlagsAndArgs()
+
+  // Initializations.
   var activeDBGpConnection net.Conn
 
-  MsgsForCmdLineUI  := make(chan message.Message)
-  MsgsForHTTPUI     := make(chan message.Message)
-  CmdsFromUI        := make(chan string)
-  bye               := make(chan struct{})
+  var MsgsForCmdLineUI, MsgsForHTTPUI chan message.Message
 
-  go cmdline.RunUI(CmdsFromUI, bye)
-  go cmdline.UpdateUIStatus(MsgsForCmdLineUI)
+  CmdsFromUI := make(chan string)
+  bye        := make(chan struct{})
 
-  go http.RunUI(CmdsFromUI)
-  go http.UpdateUIStatus(MsgsForHTTPUI)
+  // Launch all interfaces.
+  if hasCmdLine {
+    MsgsForCmdLineUI = make(chan message.Message)
 
+    go cmdline.RunUI(CmdsFromUI, bye)
+    go cmdline.UpdateUIStatus(MsgsForCmdLineUI)
+  }
+
+  if hasHTTP {
+    MsgsForHTTPUI = make(chan message.Message)
+
+    go http.Listen(docroot, CmdsFromUI)
+    go http.Tell(MsgsForHTTPUI)
+  }
+
+  // Talk to DBGp engine.
   sock := core.ListenForDBGpEngine()
   go core.RecvMsgsFromDBGpEngine(sock, &activeDBGpConnection, MsgsForCmdLineUI, MsgsForHTTPUI)
   go core.SendCmdsToDBGpEngine(&activeDBGpConnection, CmdsFromUI)
 
   <- bye
+}
+
+/**
+ * Setup command line flags and arguments.
+ *
+ * Return the values of these flags and arguments.
+ *
+ * Arg:
+ *  - docroot : Docroot of code that will be debugged.
+ *
+ * Flag:
+ *  - cmdline : We want the command line.
+ *  - nohttp  : No HTTP.
+ */
+func getFlagsAndArgs() (docroot string, hasCmdLine, hasHTTP bool) {
+
+  docrootArg     := flag.String("docroot", "", "Path of directory whose code you want to debug; e.g. /var/www/html/")
+  hasCmdLineFlag := flag.Bool("cmdline", false, "Launch command line debugger.")
+  noHTTPFlag     := flag.Bool("nohttp", false, "Do *not* launch HTTP interface of the debugger.")
+
+  flag.Parse()
+
+  docroot    = *docrootArg
+  hasCmdLine = *hasCmdLineFlag
+  hasHTTP    = ! *noHTTPFlag
+
+  return docroot, hasCmdLine, hasHTTP
 }
