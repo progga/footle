1456eeb6ae8c5122f9acdcae2d39de4422923b89
Progga <proggaprogga@gmail.com>
2018-09-01 09:04:51 +0100

    Bug fix for CSS selector escaping.
    In newer versions of Firefox, the type of the CSS object is "object" instead
    of "function".  So we now check for both.

--- src/ui/scripts/common.js
+++ src/ui/scripts/common.js
@@ -15,16 +15,18 @@
  * @see CSS.escape()
  * @see jQuery.escapeSelector()
  * @see https://learn.jquery.com/using-jquery-core/faq/how-do-i-select-an-element-by-an-id-that-has-characters-used-in-css-notation/
+ *
+ * @todo Add unit test.
  */
 function escapeSelector (selector) {
   var escapedSelector = ''
 
   // The CSS.escape() function is in Draft status.  So unavailable in some
   // browsers.
-  if (typeof CSS === 'function' && typeof CSS.escape === 'function') {
+  if ((typeof CSS === 'function' || typeof CSS === 'object') && typeof CSS.escape === 'function') {
     escapedSelector = CSS.escape(selector)
   } else {
-    escapedSelector = selector.replace(/(:|\.|\[|\]|,|=|@)/g, '\\$1')
+    escapedSelector = selector.replace(/(:|\.|\[|\]|,|=|@|%)/g, '\\$1')
   }
 
   return escapedSelector
